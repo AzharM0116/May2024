eba43c89101d81f287d438a190f9ebc62989ada8
AzharM0116 <62126452+AzharM0116@users.noreply.github.com>
2024-05-30 20:39:09 +0530

    Created Employee2 and Department 2 Tables

--- Deploy/Department_Table.sql
+++ Deploy/Department_Table.sql
@@ -1,4 +1,4 @@
 use database dev_db;
 use schema dev_db_schema;
 use role accountadmin;
-create or replace table Department_1(deptid number);
+create or replace table Department_2(deptid number);
--- Deploy/Employee_Table.sql
+++ Deploy/Employee_Table.sql
@@ -1,6 +1,6 @@
 use database dev_db;
 use schema dev_db_schema;
 use role accountadmin;
-create or replace table Employee_1(empid number);
+create or replace table Employee_2(empid number);
 
 
