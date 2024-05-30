e3c7374fe10c4c49c67648d4dc2ebbed8946dada
AzharM0116 <62126452+AzharM0116@users.noreply.github.com>
2024-05-30 20:20:07 +0530

    Employee and Department Table

--- Deploy/Department_Table.sql
+++ Deploy/Department_Table.sql
@@ -1,4 +1,4 @@
 use database dev_db;
 use schema dev_db_schema;
 use role accountadmin;
-create or replace table dept5(empid number);
+create or replace table Department_1(deptid number);
--- Deploy/Employee_Table.sql
+++ Deploy/Employee_Table.sql
@@ -1,7 +1,6 @@
 use database dev_db;
 use schema dev_db_schema;
 use role accountadmin;
-create or replace table emp5(empid number);
+create or replace table Employee_1(empid number);
 
---OPS_PURCHASING_DASHBOARD_INFO_RECORD_GENCON_20240509_SRT0997575
 
