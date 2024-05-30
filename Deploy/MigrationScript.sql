fd73998929b332600e7d822e3133d8b25caf8eec
AzharM0116 <62126452+AzharM0116@users.noreply.github.com>
2024-05-30 22:41:16 +0530

    Removed Extra content from migration script.sql file only kept file names

--- Deploy/MigrationScript.sql
+++ Deploy/MigrationScript.sql
@@ -1,14 +1,2 @@
--- Setting the Context correct for installation 
-use database dev_db;
-use schema dev_db_schema;
-use role accountadmin;
-
--- Migration script to execute table1.sql and table2.sql in order
-
--- Execute table1.sql
-table1.sql
--- Execute table2.sql
-table2.sql
-
--- changed this to be called from snowsql directly only requirement is to make sure that this file should be copied to deploy folder. we will configure this later 
--- for now move this files manually to deploy folder so that migrationscript.sql should point to this files 
+Table1.sql
+Table2.sql
